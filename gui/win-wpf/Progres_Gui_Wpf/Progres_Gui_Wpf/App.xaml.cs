6bfb37b7d9c82c19458ba6c86d66528328ef5049
Juraj Ahel <juraj.ahel@gmail.com>
2024-04-28 20:58:41 +0200

    add wpf project

---
+++ gui/win-wpf/Progres_Gui_Wpf/Progres_Gui_Wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Progres_Gui_Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
